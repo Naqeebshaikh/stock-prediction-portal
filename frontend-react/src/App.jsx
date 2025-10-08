5a847538e598aa6d252ef6f4718b11a8270a8923
Naqeeb <naqeeb786shaikh@gmail.com>
2025-10-08 15:33:59 +0530

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import './assets/css/style.css'
-import Header from './components/Header'
 import Main from './components/main'
+import { BrowserRouter,Routes,Route } from "react-router"
+import Register from './components/register'
+import Header from './components/Header'
 import Footer from './components/footer'
-
+import Login from './components/Login'
 
 
 function App() {
@@ -11,9 +13,16 @@ function App() {
 
   return (
     <>
+     <BrowserRouter>
       <Header/>
-      <Main/>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
       <Footer/>
+     </BrowserRouter>
+      
     </>
   )
 }
